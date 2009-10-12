c2f86b363b71ce8bae2240813566e49e659f72aa
dwdyer <dan@uncommons.org>
2009-10-12 21:30:33 +0000

    Improved configuration for cold decks evolution.

--- experiments/src/java/main/org/uncommons/poker/experiments/colddecks/ColdDecks.java
+++ experiments/src/java/main/org/uncommons/poker/experiments/colddecks/ColdDecks.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import org.uncommons.maths.number.ConstantGenerator;
 import org.uncommons.maths.random.DiscreteUniformGenerator;
+import org.uncommons.maths.random.Probability;
 import org.uncommons.maths.random.XORShiftRNG;
 import org.uncommons.poker.game.cards.PlayingCard;
 import org.uncommons.poker.game.cards.SevenCardHandEvaluator;
@@ -20,6 +21,7 @@ import org.uncommons.watchmaker.framework.factories.ListPermutationFactory;
 import org.uncommons.watchmaker.framework.operators.EvolutionPipeline;
 import org.uncommons.watchmaker.framework.operators.ListOrderCrossover;
 import org.uncommons.watchmaker.framework.operators.ListOrderMutation;
+import org.uncommons.watchmaker.framework.operators.Replacement;
 import org.uncommons.watchmaker.framework.selection.SigmaScaling;
 import org.uncommons.watchmaker.framework.termination.TargetFitness;
 
@@ -35,7 +37,9 @@ public class ColdDecks
         CandidateFactory<List<PlayingCard>> factory = new ListPermutationFactory<PlayingCard>(Arrays.asList(PlayingCard.values()));
         FitnessEvaluator<List<PlayingCard>> fitnessEvaluator = new ColdDeckEvaluator(new SevenCardHandEvaluator(), playerCount);
         List<EvolutionaryOperator<List<PlayingCard>>> operators = new ArrayList<EvolutionaryOperator<List<PlayingCard>>>(2);
+        operators.add(new Replacement<List<PlayingCard>>(factory, new Probability(0.005)));
         operators.add(new ListOrderCrossover<PlayingCard>());
+        //operators.add(new ListInversion<PlayingCard>(new Probability(0.1)));
         operators.add(new ListOrderMutation<PlayingCard>(new ConstantGenerator<Integer>(1),
                                                          new DiscreteUniformGenerator(1, 51, rng)));
         EvolutionaryOperator<List<PlayingCard>> pipeline = new EvolutionPipeline<List<PlayingCard>>(operators);
@@ -62,7 +66,7 @@ public class ColdDecks
 //        EvolutionMonitor<List<PlayingCard>> monitor = new EvolutionMonitor<List<PlayingCard>>();
 //        engine.addEvolutionObserver(monitor);
 //        monitor.showInFrame("Cold Decks", true);
-        List<PlayingCard> deck = engine.evolve(50, 2, new TargetFitness(0, false));
+        List<PlayingCard> deck = engine.evolve(500, 10, new TargetFitness(0, false));
         for (PlayingCard card : deck)
         {
             System.out.print(card.toString() + ",");
